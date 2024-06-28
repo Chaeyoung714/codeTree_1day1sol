d5a96d2e3e0afd2e93be3de339fe7787a28682a0
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2024-06-28 13:34:31 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240628/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int k, n;
+    public static boolean isDuplicate = false;
+    public static int[] arr;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        k = sc.nextInt();
+        n = sc.nextInt();
+
+        arr = new int[n];
+
+        chooseConditional(0);
+    }
+
+    public static void chooseConditional(int depth) {
+        if (depth > n-1) {
+            for (int elem: arr) {
+                System.out.print(elem + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int val=1; val<=k; val++) {
+            if (depth > 0 && arr[depth-1] == val) {
+                if (isDuplicate) {
+                    isDuplicate = false; //연속상태를 끊음
+                    continue;
+                } else {
+                    isDuplicate = true;
+                }
+            }
+            arr[depth] = val;
+            chooseConditional(depth+1);
+        }
+    }
+}
