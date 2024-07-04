cd1556f6deef8aeff05477c781cd961e08227302
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2024-07-04 20:22:43 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240704/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
+++ 240704/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
@@ -14,7 +14,7 @@ public class Main {
         }
 
         DP[0] = arr[0] * 2;
-        int maxSum = -Integer.MAX_VALUE;
+        int maxSum = DP[0]
         for (int i=1; i<n; i++) {
             DP[i] = Math.max(DP[i-1], 2*arr[i]);
             if (maxSum < DP[i]) {
