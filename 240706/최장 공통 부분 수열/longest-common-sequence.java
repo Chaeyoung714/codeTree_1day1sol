f2f41b581a70a1dcd4c761987d3ccc5f586eb7ac
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2024-07-06 10:29:51 +0900

    Title: 최장 공통 부분 수열, Time: 202ms, Memory: 14MB, Status: Passed - CodeTree

--- 240706/최장 공통 부분 수열/longest-common-sequence.java
+++ 240706/최장 공통 부분 수열/longest-common-sequence.java
@@ -19,48 +19,57 @@ public class Main {
         //초기화
         boolean satisfied = false;
         for (int i=0; i<lenA; i++) {
-            if (a[i].equals(b[0])) {
-                // DP[i][0] = DP[i-1][0] + 1;
-                satisfied = true;
+            if (satisfied) {
                 DP[i][0] = 1;
             } else {
-                // DP[i][0] = DP[i-1][0];
-                DP[i][0] = 0;
+                if (a[i].equals(b[0])) {
+                    // DP[i][0] = DP[i-1][0] + 1;
+                    satisfied = true;
+                    DP[i][0] = 1;
+                } else {
+                    // DP[i][0] = DP[i-1][0];
+                    DP[i][0] = 0;
+                }
             }
         }
+
+        satisfied = false;
         for (int j=0; j<lenB; j++) {
-            if (a[0].equals(b[j])) {
-                // DP[0][j] = DP[0][j-1] + 1;
-                satisfied = true;
+            if (satisfied) {
                 DP[0][j] = 1;
             } else {
-                // DP[0][j] = DP[0][j-1];
-                DP[0][j] = 0;
+                if (a[0].equals(b[j])) {
+                    // DP[0][j] = DP[0][j-1] + 1;
+                    satisfied = true;
+                    DP[0][j] = 1;
+                } else {
+                    // DP[0][j] = DP[0][j-1];
+                    DP[0][j] = 0;
+                }
             }
         }
 
         //예외 -  왜 따로 빼야하지?
-        if (lenA == 1 || lenB == 1) {
-            if (satisfied) {
-                System.out.println(1);
-            }
-            else {
-                System.out.println(0);
-            }
-        } else {
+        // if (lenA == 1 || lenB == 1) {
+        //     if (satisfied) {
+        //         System.out.println(1);
+        //     }
+        //     else {
+        //         System.out.println(0);
+        //     }
+        // } else {
             //DP채움
-            for (int i=1; i<lenA; i++) {
-                for (int j=1; j<lenB; j++) {
-                    if (a[i].equals(b[j])) {
-                        DP[i][j] = DP[i-1][j-1] + 1;
-                    } else {
-                        DP[i][j] = Math.max(DP[i-1][j], DP[i][j-1]);
-                    }
+        for (int i=1; i<lenA; i++) {
+            for (int j=1; j<lenB; j++) {
+                if (a[i].equals(b[j])) {
+                    DP[i][j] = DP[i-1][j-1] + 1;
+                } else {
+                    DP[i][j] = Math.max(DP[i-1][j], DP[i][j-1]);
                 }
             }
-
-            System.out.println(DP[lenA-1][lenB-1]);
         }
+
+        System.out.println(DP[lenA-1][lenB-1]);
 
     }
 }
