242aea14faf1f6eb7a019bcb34a50999e1fc6f0c
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2024-07-06 17:16:04 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240706/계단 오르기/climbing-stairs.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] DP = new int[n+1];
+
+        if (n <= 3) {
+            System.out.println(1);
+        } else {
+            //초기화
+            DP[1] = 0;
+            DP[2] = 1;
+            DP[3] = 1;
+
+            for (int i=1; i<=n; i++) {
+                DP[i] = DP[i-2] + DP[i-3];
+            }
+
+            System.out.println(DP[n]);
+        }
+    }
+}
