50a61087ea373b5d5c1a761cd7b624576a26fbbe
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2024-07-08 14:16:44 +0900

    Title: 아름다운 수, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240708/아름다운 수/beautiful-number.java
+++ 240708/아름다운 수/beautiful-number.java
@@ -8,7 +8,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        n = sc.nextInt();
         result = new int[n];
 
         if (n < 4) {
@@ -28,6 +28,11 @@ public class Main {
             if (lastVal == cnt) { //숫자 한묶음이 완성되었는지
                 totalCnt ++;
             }
+            // System.out.println("depth = " + depth + "lastval = " + lastVal + "cnt = " + cnt);
+            // for (int elem: result) {
+            //     System.out.print(elem + " ");
+            // }
+            // System.out.println();
             return;
         }
 
@@ -35,7 +40,7 @@ public class Main {
         if (cnt < lastVal) { //한묶음 완성이 안됐으면
             result[depth] = lastVal;
             beautifulNumber(depth+1, lastVal, cnt+1);
-            
+
         } else if (cnt == lastVal) { //한묶음 완성이 됐으면
             for (int newVal=1; newVal<=maxNum; newVal++) {
                 result[depth] = newVal;
