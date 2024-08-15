a90b9947dc8a8583e0ee86581629b27325174c3e
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2024-08-15 09:20:05 +0900

    Title: 외판원 순회, Time: 109ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240815/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n, minCost;
+    public static int[][] costs;
+    public static int[] visited;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        costs = new int[n][n];
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                costs[i][j] = sc.nextInt();
+            }
+        } 
+
+        minCost = Integer.MAX_VALUE;
+        visited = new int[n];
+        visited[0] = 1;
+        findMinCost(0, 0, 0); //0~n-1번 지점
+
+        System.out.println(minCost);
+    }
+
+    public static void findMinCost(int from, int cost, int depth) {
+        if (depth >= n-1) { //n-1번 이동 후
+            cost += costs[from][0];
+            minCost = Math.min(minCost, cost);
+            return;
+        }
+
+        for (int to=0; to<n; to++) {
+            if (visited[to] == 0) {
+                visited[to] = 1;
+                findMinCost(to, cost + costs[from][to], depth+1);
+                visited[to] = 0;
+            }
+        }
+    }
+}
